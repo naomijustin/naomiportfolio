d6adca45e892a3039cf46674eb010a35e259a586
Naomi Justin <63893978+naomijustin@users.noreply.github.com>
2020-12-12 23:32:52 +1100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $toEmail = "test@test.com";
-    $mailHeaders = "From: " . $_POST["inputName"] . "<". $_POST["inputEmail"] .">\r\n";
-    if(mail($toEmail, $_POST["inputMessage"], $mailHeaders)) {
-        print "<p class='success'>Mail Sent.</p>";
-    } else {
-        print "<p class='error'>Problem sending message</p>";
-    }
-?>
