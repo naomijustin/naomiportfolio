e6b274d9f10fdc192992989a730a9fb2e4ecbff3
Naomi Justin <63893978+naomijustin@users.noreply.github.com>
2020-12-13 23:05:24 +1100

    2020-12-13_1_HeaderNavIssues
    - Issue 1: Desktop Nav disappears on keydown
    >> Found a document keydown command that was being used to close the hamburger menu on Esc. When disabling, this issue was resolved (testing purposes).
    >> Re-wrote function so it did not affect Desktop Header Nav > Was not checking the keycode correctly > Added to make sure all checks were equally keycode event 27 ("Esc")
    - Issue 2: After resizing browser from mobile back to desktop, desktop Nav disappears (even when initially displayed)
    >> Add NavContainer and NavContainer a "display: inline;" to all media queries above 601 pixels > No change
    >> Found that "NavContainter a" had not been specified in the min 601px media query. Added this and added !important to both container and links. Tested and seems to work so far. Issue no longer present

--- php/contact.php
+++ php/contact.php
@@ -1,19 +1,27 @@
 <?php 
 
 if (isset($_POST['submit'])) {
+    // Form Data
     $name = $_POST['name'];
     $subject = $_POST['subject'];
     $mailFrom = $_POST['email'];
     $message = $_POST['message'];
-
-    $mailTo = "test@test.com";
+    
+    // Send form data via email
+    $mailTo = "test@test.com"; //Removed email for privacy
     $headers = "From: ".$mailFrom;
     $txt = "You have received an email from ".$name.".\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
-    // header("Location: /php/contact.html?mailsend");
+    // header("Location: /php/contact.html?mailsend=success");
     //echo "<script>alert('Your message has been sent');window.location.href='/php/contact.html'</script>";
     echo "<script>window.location.href='/php/contact.html';alert('Your message has been sent')</script>";
+    exit();
 }
+// else {
+//     // Redirect to home page if accessing without 'Submit'
+//     header("Location: ../contact.html?mailsend=error");
+//     exit();
+// }
 
 ?>
