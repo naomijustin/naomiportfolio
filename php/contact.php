96873215e674d2573e9c360c1494d53bedeae839
Naomi Justin <63893978+naomijustin@users.noreply.github.com>
2021-01-14 23:08:03 +1100

    2021-01-14_3_Tweaks
    Readme
    - Add heading that was missing
    - Fixed spelling errors
    
    htaccess
    - Add comment at top of .htaccess
    
    PHP
    - Updated Errors and message sent to give more context of message and layout when sending from Contact Form
    
    HeaderNav / CSS
    - Bug: Issue with carousel being infront of hamburger menu on Mobile View (and not fixed)
    >> Fixed via changing to fixed postion in media query for mobile menu fade and nav bar

--- php/contact.php
+++ php/contact.php
@@ -8,14 +8,21 @@ if (isset($_POST['submit'])) {
     $message = $_POST['message'];
     
     // Send form data via email
-    $mailTo = "test@test.com"; 
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an email from ".$name.".\n\n".$message;
+    $mailTo = "test@test.com"; // Removing email for privacy reasons
+    $headers = "Contact Form | From: ".$mailFrom." | naomijustin.com";
+    $txt = "You have received an email from ".$name." (via the naomijustin.com contact form).\n\n.Email: ".$mailFrom.".\n\n".$message;
 
-    mail($mailTo, $subject, $txt, $headers);
+    $result = mail($mailTo, $subject, $txt, $headers);
+    
+    if(!$result) {
+        echo "<script>window.location.href='/pages/contact.html';alert('Error sending message, please try again')</script>";
+    }
+    else {
+        echo "<script>window.location.href='/pages/contact.html';alert('Your message has been sent')</script>";
+    }
     
     //Correct Script (for deploy)
-    echo "<script>window.location.href='/pages/contact.html';alert('Your message has been sent')</script>";
+    //echo "<script>window.location.href='/pages/contact.html';alert('Your message has been sent')</script>";
     
     // Test Script
     // echo "<script>window.location.href='/php/contact.html';alert('Your message has been sent')</script>";
