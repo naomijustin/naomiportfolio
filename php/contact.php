d01d0a0db37f162592ac39dc89dee5dd53fae422
Naomi Justin <63893978+naomijustin@users.noreply.github.com>
2021-01-16 15:26:51 +1100

    2021-01-16_2_Prod_WebIssues_Deploy
    HeaderNav
    - Change "Resume" link to open in a new tab instead of download
    
    PHP
    - Removed error handlers as issue occurring 
    - Slightly changed PHP mail layout
    
    Work, About, Contact, PageNotFound
    - Moved Loader above all other elements on page
    
    Index
    - Fade in Header Nav on window load

--- php/contact.php
+++ php/contact.php
@@ -10,17 +10,12 @@ if (isset($_POST['submit'])) {
     // Send form data via email
     $mailTo = "test@test.com"; // Removing email for privacy reasons
     $headers = "Contact Form | From: ".$mailFrom." | naomijustin.com";
-    $txt = "You have received an email from ".$name." (via the naomijustin.com contact form).\n\n.Email: ".$mailFrom.".\n\n".$message;
+    $txt = "You have received an email from ".$name." (via the naomijustin.com contact form).\n\nEmail: ".$mailFrom."\n\n".$message;
 
-    $result = mail($mailTo, $subject, $txt, $headers);
-    
-    if(!$result) {
-        echo "<script>window.location.href='/pages/contact.html';alert('Error sending message, please try again')</script>";
-    }
-    else {
-        echo "<script>window.location.href='/pages/contact.html';alert('Your message has been sent')</script>";
-    }        
+    mail($mailTo, $subject, $txt, $headers);
 
+    echo "<script>window.location.href='/pages/contact.html';alert('Your message has been sent')</script>";
+        
     exit();
 }
 
